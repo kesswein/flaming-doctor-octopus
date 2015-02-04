d6fb2686afa8495873c753044f1e4f6796ac8a6f
Glenn Downing <downing@cs.utexas.edu>
2015-02-03 19:04:09 -0600

    another commit

--- exercises/Factorial.py
+++ exercises/Factorial.py
@@ -6,27 +6,27 @@
 
 from unittest  import main, TestCase
 
-def factorial_recursion (n) :
+def factorial (n) :
     ...
 
 class MyUnitTests (TestCase) :
     def test_0 (self) :
-        self.assertEqual(factorial_recursion(0), 1)
+        self.assertEqual(factorial(0), 1)
 
     def test_1 (self) :
-        self.assertEqual(factorial_recursion(1), 1)
+        self.assertEqual(factorial(1), 1)
 
     def test_2 (self) :
-        self.assertEqual(factorial_recursion(2), 2)
+        self.assertEqual(factorial(2), 2)
 
     def test_3 (self) :
-        self.assertEqual(factorial_recursion(3), 6)
+        self.assertEqual(factorial(3), 6)
 
     def test_4 (self) :
-        self.assertEqual(factorial_recursion(4), 24)
+        self.assertEqual(factorial(4), 24)
 
     def test_5 (self) :
-        self.assertEqual(factorial_recursion(5), 120)
+        self.assertEqual(factorial(5), 120)
 
 if __name__ == "__main__" :
     main()
